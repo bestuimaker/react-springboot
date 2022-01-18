ee5b98b4c7d5b9e329cff27cdfff51fcb657edcc
Bhagwati Pandey <Bhagwati.Pandey@NIIT-Tech.com>
2022-01-18 23:57:28 +0530

    Connected front-end to backend implemented the axios HTTP Client created Dashboard  is getting data from spring boot

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# react-springboot
+# React Springboot
 To put React SPA inside a war file  of Springboot and run it in the tomcat.
--- motor-demo/src/shared/components/not-found/not-found.js
+++ motor-demo/src/shared/components/not-found/not-found.js
@@ -1,6 +1,6 @@
 function NotFound() {
     return (
-        <div>
+        <div className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             <h2>Nothing to see here!</h2>
         </div>
     );
