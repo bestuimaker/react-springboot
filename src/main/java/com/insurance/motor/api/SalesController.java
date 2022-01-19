5aa8214c848a45b00684367d0bb6c14a71cc2844
Bhagwati Pandey <Bhagwati.Pandey@NIIT-Tech.com>
2022-01-19 15:21:29 +0530

    Sales Order controller modified

--- src/main/java/com/insurance/motor/api/SalesController.java
+++ src/main/java/com/insurance/motor/api/SalesController.java
@@ -4,12 +4,16 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class SalesController {
 	@CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping("/sales")
-	public Sales sales() {
-		return new Sales();
+	public List<SalesOrder> GetAllSalesOrder() {
+		List<SalesOrder> salesOrders = List.of(
+				new SalesOrder("6540012121", "Food Delivery", "20122022", "Bhagwati", "Bhag0987", "350", "Cupcake")
+		);
+		return salesOrders;
 	}
-
 }
