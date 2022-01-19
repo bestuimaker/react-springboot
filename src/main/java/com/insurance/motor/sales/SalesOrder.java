ede9858a9801d569313af889c26f17e061560560
Bhagwati Pandey <Bhagwati.Pandey@NIIT-Tech.com>
2022-01-19 22:21:26 +0530

    Refactored and removed duplicate properties

--- src/main/java/com/insurance/motor/sales/SalesOrder.java
+++ src/main/java/com/insurance/motor/sales/SalesOrder.java
@@ -4,22 +4,22 @@ import java.util.List;
 
 public class SalesOrder {
 
-	public int OrderID;
-	public String OrderName;
-	public String OrderDate;
-	public String CustomerName;
-	public int CustomerID;
-	public int OrderTotal;
-	public List<Products> OrderItem;
+	public int orderID;
+	public String orderName;
+	public String orderDate;
+	public String customerName;
+	public int customerID;
+	public int orderTotal;
+	public List<Product> products;
 
 	public SalesOrder(int orderID, String orderName, String orderDate, String customerName, int customerID,
-			int orderTotal, List<Products> orderItem) {
-		OrderID = orderID;
-		OrderName = orderName;
-		OrderDate = orderDate;
-		CustomerName = customerName;
-		CustomerID = customerID;
-		OrderTotal = orderTotal;
-		OrderItem = orderItem;
+			int orderTotal, List<Product> products) {
+		this.orderID = orderID;
+		this.orderName = orderName;
+		this.orderDate = orderDate;
+		this.customerName = customerName;
+		this.customerID = customerID;
+		this.orderTotal = orderTotal;
+		this.products = products;
 	}
 }
--- src/main/java/com/insurance/motor/sales/SalesOrderController.java
+++ src/main/java/com/insurance/motor/sales/SalesOrderController.java
@@ -8,39 +8,10 @@ import java.util.List;
 
 @RestController
 public class SalesOrderController {
+
 	@CrossOrigin(origins = "http://localhost:3000")
-	@GetMapping("/sales")
+	@GetMapping("/sales")	
 	public List<SalesOrder> GetAllSalesOrder() {
-		List<SalesOrder> salesOrders = List.of(
-				new SalesOrder(
-						3232, 
-						"Food Delivery", 
-						"20122022", 
-						"Bhagwati", 
-						12345, 
-						350, 
-						List.of(
-								new Products 
-								(12454, "Cup Cake", 2),
-								new Products 
-								(12434, "Milk Cake", 1)
-								)
-						),
-				new SalesOrder(
-						1234, 
-						"Food Delivery", 
-						"20122022", 
-						"Bhagwati", 
-						4563, 
-						350, 
-						List.of(
-								new Products 
-								(12454, "Sandwitch", 2),
-								new Products 
-								(12434, "Banana Shake", 1)
-								)
-						)
-		);
-		return salesOrders;
+		return SalesOrderService.GetAllSalesOrder();
 	}
 }
