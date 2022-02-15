46cfc85426369770f0b7297d108f645e3a1dbfe1
Bhagwati Pandey <84673546+bestuimaker@users.noreply.github.com>
2022-02-16 01:34:51 +0530

    Update App.js

--- motor-demo/src/App.js
+++ motor-demo/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Dashboard from './core/components/dashboard/dashboard';
 import Reports from './core/components/reports/reports';
 import Layout from './core/components/layout/layout';
